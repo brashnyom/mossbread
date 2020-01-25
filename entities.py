bfb17b9058c470b8336b33a1c4dc4317d0b59347
brittleshinpass <brittleshinpass@protonmail.com>
2020-01-25 20:09:50 +0200

    Added EntityHandler

--- entities.py
+++ entities.py
@@ -1,4 +1,45 @@
+from typing import List
+
+from pyglet.sprite import Sprite
+
 from rendering import Rendering
+from game_map import GameMap
+
+
+class EntityHandler:
+    def __init__(self, game_map: GameMap, rendering: Rendering):
+        self.entities: List[Entity] = list()
+        self.game_map = game_map
+        self.rendering = rendering
+
+    def spawn_entity(self, x: int, y: int, tile: int):
+        self.entities.append(
+            Entity(x, y, Sprite(self.rendering.get_tile_as_image(tile), 0, 0))
+        )
+
+    def move_entity(self, entity_idx: int, x: int, y: int):
+        target_entity = self.entities[entity_idx]
+        potential_map_tile = self.game_map.get(
+            target_entity.x + x, self.game_map.height - (target_entity.y + y + 1)
+        )
+        if potential_map_tile != 1:
+            return
+
+        for idx, entity in enumerate(self.entities):
+            if idx == entity_idx:
+                continue
+            if (target_entity.x + x) == entity.x and (target_entity.y + y) == entity.y:
+                return
+
+        target_entity.x += x
+        target_entity.y += y
+
+    def update_entities(self):
+        for entity in self.entities:
+            entity.update_sprite_pos(
+                *self.rendering.relative_to_camera(entity.x, entity.y)
+            )
+            entity.sprite.draw()
 
 
 class Entity:
--- main.py
+++ main.py
@@ -1,55 +1,33 @@
 import pyglet
 from pyglet.window import key
-from pyglet.sprite import Sprite
 from rendering import Rendering
 from game_map import GameMap
 
-from entities import Entity
+from entities import EntityHandler
 
 
 game_map = GameMap.from_file("sample_map.txt")
 rendering = Rendering(game_map)
+entity_handler = EntityHandler(game_map, rendering)
 
-player_image = rendering.get_tile_as_image(5)
+entity_handler.spawn_entity(1, 1, 5)
+entity_handler.spawn_entity(15, 15, 5)
 
-player = Entity(1, 1, Sprite(player_image, 0, 0))
-npc = Entity(15, 10, Sprite(player_image, 0, 0))
-
-entities = list()
-entities.append(player)
-entities.append(npc)
+player = entity_handler.entities[0]
 
 rendering.center_camera(player.x, player.y)
-
-
-def move_entity(entity_idx: int, x: int, y: int):
-    target_entity = entities[entity_idx]
-    potential_map_tile = game_map.get(
-        target_entity.x + x, game_map.height - (target_entity.y + y + 1)
-    )
-    if potential_map_tile != 1:
-        return
-
-    for idx, entity in enumerate(entities):
-        if idx == entity_idx:
-            continue
-        if (target_entity.x + x) == entity.x and (target_entity.y + y) == entity.y:
-            return
-
-    target_entity.x += x
-    target_entity.y += y
 
 
 @rendering.window.event
 def on_key_press(symbol, modifiers):
     if symbol == key.W:
-        move_entity(0, 0, 1)
+        entity_handler.move_entity(0, 0, 1)
     elif symbol == key.S:
-        move_entity(0, 0, -1)
+        entity_handler.move_entity(0, 0, -1)
     elif symbol == key.D:
-        move_entity(0, 1, 0)
+        entity_handler.move_entity(0, 1, 0)
     elif symbol == key.A:
-        move_entity(0, -1, 0)
+        entity_handler.move_entity(0, -1, 0)
     rendering.center_camera(player.x, player.y)
 
 
@@ -57,9 +35,7 @@ def on_key_press(symbol, modifiers):
 def on_draw():
     rendering.window.clear()
     rendering.draw_map()
-    for entity in entities:
-        entity.update_sprite_pos(*rendering.relative_to_camera(entity.x, entity.y))
-        entity.sprite.draw()
+    entity_handler.update_entities()
 
 
 pyglet.app.run()
