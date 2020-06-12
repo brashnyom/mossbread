bc81dd2de7d74830d17e3164c939c9cb82dd5478
brittleshinpass <brittleshinpass@protonmail.com>
2020-06-12 17:07:55 +0300

    Add some structure to collision checks

--- entities.py
+++ entities.py
@@ -3,6 +3,14 @@ from typing import Dict
 from game_map import GameMap
 
 
+class Entity:
+    def __init__(self, id: int, x: int, y: int, tile_id: int):
+        self.id = id
+        self.x = x
+        self.y = y
+        self.tile_id = tile_id
+
+
 class EntityHandler:
     def __init__(self, game_map: GameMap):
         self.entities: Dict[int, Entity] = dict()
@@ -21,26 +29,32 @@ class EntityHandler:
     def move_entity(self, target_entity_id: int, x: int, y: int):
         # TODO Should this method work by incrementing the target entity's
         # coordinates or directly setting them to the requested position?
-        # TODO Add other ways of resolving collisions instead of preventing
-        # movement (for example, by damaging one of the entities)
         target_entity = self.entities[target_entity_id]
-
-        if not self.game_map.traversable(target_entity.x + x, target_entity.y + y):
-            return
-
-        for entity_id, entity in self.entities.items():
-            if entity_id == target_entity_id:
-                continue
-            if (target_entity.x + x) == entity.x and (target_entity.y + y) == entity.y:
-                return
 
         target_entity.x += x
         target_entity.y += y
 
+        if self.handle_collision_terrain(target_entity):
+            target_entity.x -= x
+            target_entity.y -= y
 
-class Entity:
-    def __init__(self, id: int, x: int, y: int, tile_id: int):
-        self.id = id
-        self.x = x
-        self.y = y
-        self.tile_id = tile_id
+        for entity_id, entity in self.entities.items():
+            if entity_id == target_entity_id:
+                continue
+            elif self.check_collision(target_entity, entity):
+                if self.handle_collision_entity(target_entity, entity):
+                    target_entity.x -= x
+                    target_entity.y -= y
+                    break
+
+    def check_collision(self, l_entity: Entity, r_entity: Entity) -> bool:
+        return l_entity.x == r_entity.x and l_entity.y == r_entity.y
+
+    def handle_collision_terrain(self, entity: Entity) -> bool:
+        # It is assumed that the entity is colliding with the terrain on
+        # the tile it is currently standing on
+        return not self.game_map.traversable(entity.x, entity.y)
+
+    def handle_collision_entity(self, l_entity: Entity, r_entity: Entity) -> bool:
+        # l_entity is the entity which initiated the collision
+        return True
